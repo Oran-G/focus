286eb9ded2e0aac9b7a5b6e9968b2a20635472e3
Oran-G <31713576+Oran-G@users.noreply.github.com>
2021-10-19 16:11:17 -0400

    10/19 data analysis

--- parse.py
+++ parse.py
@@ -13,17 +13,23 @@ from tqdm import tqdm
 @hydra.main(config_path='configs', config_name='defaults')
 def main(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
-
+    '''
+    In the meantime, you can get started on some data analysis. Make plots of the following:
+        - number of sequences in REBASE
+        - fraction with DNA labels, fraction that don’t have 
+        - what the DNA labels look like (are they all pretty similar?)
+    '''
     with open(cfg.io.input, 'r') as f:
         coke = f.readlines()
         df = pd.DataFrame(columns=['name', 'seq', 'bind'])
         start = False
         seq, name, bind = '', '', ''
+        total, binds = 0, 0
         for line in tqdm(coke):
             if line == '\n':
                 if start ==  True:
                     # print(seq, name, bind)
-                    df = df.append({'name':name, 'seq':seq, 'bind': bind if bind != '' else None}, ignore_index=True)
+                    df = df.append({'name':name, 'seq':seq, 'bind': bind.replace(',','') if bind != '' else None}, ignore_index=True)
                     
                     seq = ''
                     name = ''
@@ -35,6 +41,9 @@ def main(cfg: DictConfig) -> None:
                 name = l[0].replace('>','')
                 bind = l[3]
                 start = True
+                total+=1
+                if bind !='':
+                    binds+=1
                 # print(line.split(' '))
                 # if bind >5:
                 #     quit()
@@ -43,7 +52,10 @@ def main(cfg: DictConfig) -> None:
                 seq+=line.replace(' ','').replace('\n','')
                 
         table = pa.Table.from_pandas(df)
-        pq.write_table(table, 'args.output')
+        print('Total:', total)
+        print('Contains DNA bind site:', binds)
+        pq.write_table(table, cfg.io.output)
+
         f.close() 
 
 
