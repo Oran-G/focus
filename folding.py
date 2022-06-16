d8019e3bea8ef7ee608922937ab1a4f5c9117ccb
Oran-G <31713576+Oran-G@users.noreply.github.com>
2022-06-16 11:28:52 -0400

    get rid of set_trace() calls

--- folding.py
+++ folding.py
@@ -1,4 +1,4 @@
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
@@ -7,22 +7,22 @@ from fairseq.data import FastaDataset, EncodedFastaDataset, Dictionary, BaseWrap
 from constants import tokenization, neucleotides
 from torch.utils.data import DataLoader, Dataset
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 from omegaconf import DictConfig, OmegaConf
 import hydra
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 import torchmetrics
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 from typing import List, Dict
 from pytorch_lightning.loggers import WandbLogger
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 from pandas import DataFrame as df
 import pandas as pd
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
-import torch
+# import torch
 import pandas as pd
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 import esm.inverse_folding
 import esm
